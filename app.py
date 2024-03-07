3609439f54b021ddfc68d9b51fc9161a822eb726
Malisha Rasiru <145491693+Malisha-Kali@users.noreply.github.com>
2024-03-07 10:46:51 +0530

    Updated app.py file

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 logging.info('Flask server started')
 
 data = dict()
-reviews = []
-positive = 0
-negative = 0
+mails = []
+ham = 0
+spam = 0
 
 @app.route("/")
 def index():
-    data['reviews'] = reviews
-    data['positive'] = positive
-    data['negative'] = negative
+    data['mails'] = mails
+    data['ham'] = ham
+    data['spam'] = spam
 
     logging.info('========== Open home page ============')
 
@@ -35,14 +35,14 @@ def my_post():
     prediction = get_prediction(vectorized_txt)
     logging.info(f'Prediction : {prediction}')
 
-    if prediction == 'negative':
-        global negative
-        negative += 1
+    if prediction == 'spam':
+        global spam
+        spam += 1
     else:
-        global positive
-        positive += 1
+        global ham
+        ham += 1
     
-    reviews.insert(0, text)
+    mails.insert(0, text)
     return redirect(request.url)
 
 if __name__ == "__main__":
